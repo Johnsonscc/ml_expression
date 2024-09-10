cc3b6733df5a5e392e37a6722774d97f4c209a7b
sccilu <93533134+sccilu@users.noreply.github.com>
2024-09-10 12:53:05 +0800

    Initial commit

---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# pak fikri 2 > 2024-07-23 3:58am
+https://universe.roboflow.com/rabbani/pak-fikri-2
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ yolo_train.py
@@ -0,0 +1,27 @@
+import torch
+from ultralytics import YOLO
+import os
+
+def train_model():
+    # 设置CUDA环境
+    device = torch.device("cuda")
+    print(f"Using GPU: {device}")
+
+    model = YOLO('yolov8s-p6.yaml').load('yolov8s.pt')
+    # 进行模型训练
+    model.train(
+        # 从头开始训练
+        data='data.yaml',
+        epochs=100,
+        imgsz=640,
+        device=device,
+        workers = 4,
+        batch =32,
+    )
+    # 进行模型验证
+    model.val()
+if __name__ == "__main__":
+    # 调用训练函数
+    train_model()
+
+    #  python yolo_train.py
