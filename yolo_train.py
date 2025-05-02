06b511b9a568a38c235c20861ad070a8643b9f33
sccilu <93533134+sccilu@users.noreply.github.com>
2025-05-02 20:29:44 +0800

    采用yolov8s模型

--- yolo_train.py
+++ yolo_train.py
@@ -6,16 +6,16 @@ def train_model():
     # 设置CUDA环境
     device = torch.device("cuda")
     # model = YOLO('last01.pt')  # 加载网络结构
-    model = YOLO('yolov8l-p6.yaml').load('yolov8l.pt')
+    model = YOLO('yolov8s.yaml').load('yolov8s.pt')
     # 进行模型训练
     model.train(
         # 从头开始训练
         data='data.yaml',
-        epochs=200,
+        epochs=100,
         imgsz=640,
         device=device,
-        workers =0,
-        batch =16,
+        workers =4,
+        batch =128,
     )
     # 进行模型验证
     model.val()
