befd3ac4d2164aec8959898bc32a09360d3e33b9
sccilu <93533134+sccilu@users.noreply.github.com>
2025-05-04 11:04:38 +0800

    yolov8n-p2级别

--- yolo_train.py
+++ yolo_train.py
@@ -6,7 +6,7 @@ def train_model():
     # 设置CUDA环境
     device = torch.device("cuda")
     # model = YOLO('last01.pt')  # 加载网络结构
-    model = YOLO('yolov8s-p2.yaml').load('yolov8s.pt')
+    model = YOLO('yolov8n-p2.yaml').load('yolov8n.pt')
     # 进行模型训练
     model.train(
         # 从头开始训练
@@ -15,7 +15,7 @@ def train_model():
         imgsz=640,
         device=device,
         workers =4,
-        batch =32,
+        batch =64,
     )
     # 进行模型验证
     model.val()
